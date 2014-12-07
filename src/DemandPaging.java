ebe13d0bfc6daf47ea535b0e08111c8168454725
miguel5 <miguel.amigot@gmail.com>
2014-12-07 18:26:39 -0500

    Groundwork for main program

--- src/DemandPaging.java
+++ src/DemandPaging.java
@@ -1,8 +1,64 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-public class DemandPaging {
+public final class DemandPaging {
+
+	public enum ReplacementAlgorithm {
+		FIFO, RANDOM, LRU
+	}
+
+	public static final int RR_QUANTUM = 3;
+
+	public static String randomNumbersPath = "inputs/random-numbers.txt";
+	public static Scanner scanner;
+
+	public ReplacementAlgorithm algo;
+	public int machineSize;
+	public int pageSize;
+	public int processSize;
+	public int jobMix;
+	public int refsPerProcess;
+
+	public DemandPaging(ReplacementAlgorithm algo, int machineSize,
+			int pageSize, int processSize, int jobMix, int refsPerProcess)
+			throws FileNotFoundException {
+
+		this.algo = algo;
+		this.machineSize = machineSize;
+		this.pageSize = pageSize;
+		this.processSize = processSize;
+		this.jobMix = jobMix;
+		this.refsPerProcess = refsPerProcess;
+
+		DemandPaging.scanner = new Scanner(new File(
+				DemandPaging.randomNumbersPath));
+
+	}
+
+	public static int getRand() {
+		while (DemandPaging.scanner.hasNextInt())
+			return DemandPaging.scanner.nextInt();
+
+		return 0;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		ReplacementAlgorithm algo = ReplacementAlgorithm.LRU;
+		int machineSize = 1;
+		int pageSize = 1;
+		int processSize = 1;
+		int jobMix = 1;
+		int refsPerProcess = 1;
+
+		try {
+			new DemandPaging(algo, machineSize, pageSize, processSize, jobMix,
+					refsPerProcess);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 
 	}
 
