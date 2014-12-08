8bbd63b8132bc65fc1bae38ae03cc2eae787ff7c
miguel5 <miguel.amigot@gmail.com>
2014-12-07 19:20:14 -0500

    Formatting

--- src/Frame.java
+++ src/Frame.java
@@ -1,20 +1,27 @@
-
 public class Frame {
-	
+
 	private int processID;
 	private int pageNumber;
-	
-	public Frame(){
+
+	public Frame() {
 		this.processID = 0;
 		this.pageNumber = 0;
 	}
-	
-	public int getProcessID(){ return this.processID; }
-	
-	public int getPageNumber(){ return this.pageNumber; }
-	
-	public void setProcessID(int ID){ this.processID = ID; }
-	
-	public void setPageNumber(int page){ this.pageNumber = page; }
+
+	public int getProcessID() {
+		return this.processID;
+	}
+
+	public int getPageNumber() {
+		return this.pageNumber;
+	}
+
+	public void setProcessID(int ID) {
+		this.processID = ID;
+	}
+
+	public void setPageNumber(int page) {
+		this.pageNumber = page;
+	}
 
 }
--- src/JobMixProbability.java
+++ src/JobMixProbability.java
@@ -1,21 +1,25 @@
-
 public class JobMixProbability {
-	
+
 	private int A;
 	private int B;
 	private int C;
-	
-	public JobMixProbability(int A, int B, int C){
+
+	public JobMixProbability(int A, int B, int C) {
 		this.A = A;
 		this.B = B;
 		this.C = C;
 	}
-	
-	public int getA(){ return this.A; }
-	
-	public int getB(){ return this.B; }
 
-	public int getC(){ return this.C; }
+	public int getA() {
+		return this.A;
+	}
 
+	public int getB() {
+		return this.B;
+	}
+
+	public int getC() {
+		return this.C;
+	}
 
 }
