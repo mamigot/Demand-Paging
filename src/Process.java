9cdff2c2188e8f3fb5d740866f0c88f5091ee391
miguel5 <miguel.amigot@gmail.com>
2014-12-07 19:20:05 -0500

    Formatting + support for stats

--- src/Process.java
+++ src/Process.java
@@ -1,30 +1,61 @@
-
 public class Process {
 
 	private int ID;
 	private int size;
-	
 	private JobMixProbability jobMix;
+
 	private int remainingReferences;
 	private int currentWord;
-	
-	public Process(int ID, int size, JobMixProbability jobMix){
+
+	private int countPageFaults;
+	private int runningSumPageResidencyTime;
+
+	public Process(int ID, int size, JobMixProbability jobMix) {
 		this.ID = ID;
 		this.size = size;
-		
 		this.jobMix = jobMix;
+
 		this.remainingReferences = 0;
 		this.currentWord = 0;
+
+		this.countPageFaults = 0;
+		this.runningSumPageResidencyTime = 0;
 	}
-	
-	public int getID(){ return this.ID; }
-	
-	public int getSize(){ return this.size;	}
-	
-	public int getCurrentWord(){ return this.currentWord; }
-	
-	public void setCurrentWord(int word){ this.currentWord = word; }
-	
-	public int decRemainingReferences(){ return --this.remainingReferences; }
-	
+
+	public int getID() {
+		return this.ID;
+	}
+
+	public int getSize() {
+		return this.size;
+	}
+
+	public JobMixProbability getJobMix() {
+		return this.jobMix;
+	}
+
+	public int getCurrentWord() {
+		return this.currentWord;
+	}
+
+	public int getCountPageFaults() {
+		return this.countPageFaults;
+	}
+
+	public void setCurrentWord(int word) {
+		this.currentWord = word;
+	}
+
+	public int decRemainingReferences() {
+		return --this.remainingReferences;
+	}
+
+	public int incCountPageFaults() {
+		return ++this.countPageFaults;
+	}
+
+	public int logPageResidencyTime(int pageResidencyTime) {
+		return this.runningSumPageResidencyTime += pageResidencyTime;
+	}
+
 }
